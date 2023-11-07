904264f69cc23c3c342215f6c7b60c8337297cac
AriirauF <ariirau.fucks@epitech.eu>
2023-11-07 17:02:21 +0100

    Pushing

--- Main.java
+++ Main.java
@@ -1,12 +1,15 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
-public class Main extends JFrame{
+public class Main extends JFrame implements KeyListener {
+
+    JLabel label;
     public Main() {
         GridBagLayout grid = new GridBagLayout();
         this.setLayout(grid);
 
-        PanelTools p1;
         JPanel p2;
 
         GridBagConstraints c = new GridBagConstraints();
@@ -15,14 +18,16 @@ public class Main extends JFrame{
         c.weighty = 0.2;
         c.gridy = 0;
 
-        p1 = new PanelTools();
-        this.getContentPane().add(p1, c);
 
         p2 = new JPanel();
         c.weighty = 0.6;
         c.gridy = 1;
         getContentPane().add(p2,c);
 
+        ImageIcon image = new ImageIcon("pacman.png");
+        label = new JLabel(image);
+
+        p2.add(label);
         pack();
         validate();
 
@@ -30,10 +35,38 @@ public class Main extends JFrame{
         setVisible(true);
         setSize(800, 800);
         setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setFocusable(true);
+        addKeyListener(this);
     }
 
 
     public static void main(String[] args) {
-    new Main();
+        new Main();
     }
+
+    @Override
+    public void keyTyped(KeyEvent keyEvent) { }
+
+    @Override
+    public void keyPressed(KeyEvent keyEvent) {
+        int keyCode = keyEvent.getKeyCode();
+
+        if(keyCode == KeyEvent.VK_UP){
+            label.setLocation(label.getX(), label.getY() - 10);
+        }
+        if(keyCode == KeyEvent.VK_DOWN){
+            label.setLocation(label.getX(), label.getY() + 10);
+        }
+        if(keyCode == KeyEvent.VK_LEFT){
+            label.setLocation(label.getX()-10, label.getY());
+        }
+        if(keyCode == KeyEvent.VK_RIGHT){
+            label.setLocation(label.getX()+10, label.getY());
+        }
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent keyEvent) { }
 }
--- PanelTools.java
+++ PanelTools.java
@@ -1,9 +1,51 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class PanelTools extends JPanel {
+
+public class PanelTools extends JPanel implements ActionListener {
     public PanelTools() {
         super();
         this.setBackground(Color.BLUE);
+        this.setLayout(new BorderLayout());
+
+        JPanel panel = new JPanel();
+
+        panel.setLayout(new BorderLayout());
+
+        JButton b1 = new JButton("Haut");
+        b1.addActionListener(this);
+        JButton b2 = new JButton("Bas");
+        b2.addActionListener(this);
+        JButton b3 = new JButton("Gauche");
+        b3.addActionListener(this);
+        JButton b4 = new JButton("Droite");
+        b4.addActionListener(this);
+
+        panel.add(b1, BorderLayout.NORTH);
+        panel.add(b2, BorderLayout.SOUTH);
+        panel.add(b3, BorderLayout.WEST);
+        panel.add(b4, BorderLayout.EAST);
+
+        this.add(panel, BorderLayout.WEST);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent actionEvent) {
+        switch(actionEvent.getActionCommand()){
+            case "Haut":
+                System.out.println("Haut");
+                break;
+            case "Bas":
+                System.out.println("Bas");
+                break;
+            case "Gauche":
+                System.out.println("Gauche");
+                break;
+            case "Droite":
+                System.out.println("Droite");
+                break;
+        }
     }
 }
