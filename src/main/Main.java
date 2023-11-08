476bb5e0cad44973bc5da5048f06dded9ed3e3b1
AriirauF <ariirau.fucks@epitech.eu>
2023-11-08 17:36:45 +0100

    add: add main

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,5 +1,11 @@
 package src.main;
+import src.main.mvc.view.frames.MenuFrame;
+
+import java.awt.*;
+import java.io.IOException;
 
 public class Main {
-  
+    public static void main(String[] args) throws IOException, FontFormatException {
+        new MenuFrame();
+    }
 }
