6da6521019f13c86a211334a1783534c7a94be13
AriirauF <ariirau.fucks@epitech.eu>
2023-11-09 14:03:24 +0100

    feat: add hud panel

---
+++ src/main/mvc/view/panels/Game/HudPanel.java
@@ -0,0 +1,11 @@
+package src.main.mvc.view.panels.Game;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class HudPanel extends JPanel {
+    public HudPanel(){
+        this.setBackground(Color.RED);
+        this.setPreferredSize(new Dimension(800, 170));
+    }
+}
