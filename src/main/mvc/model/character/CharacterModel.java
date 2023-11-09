26db8cc8489a56d0382eae379d564ec6654e3014
LoubatTh <thomasloubat1@gmail.com>
2023-11-09 20:43:12 +0100

    docs(character): add models javadoc

--- src/main/mvc/model/character/CharacterModel.java
+++ src/main/mvc/model/character/CharacterModel.java
@@ -2,6 +2,9 @@ package src.main.mvc.model.character;
 
 import java.awt.Point;
 
+/**
+ * An abstract class representing a character in a 2D space with a position and movement methods.
+ */
 public abstract class CharacterModel {
   private Point position;
 
@@ -9,23 +12,37 @@ public abstract class CharacterModel {
     this.position = position;
   }
 
-
+  /**
+   * Represents a point in 2D space with x and y coordinates.
+   */
   public Point getPosition() {
     return this.position;
   }
 
+  /**
+   * Moves the character up by decrementing its Y coordinate.
+   */
   public void moveUp() {
     this.position.setLocation(this.position.getX(), this.position.getY() - 1);
   }
 
+  /**
+   * Moves the character down by incrementing its Y coordinate.
+   */
   public void moveDown() {
     this.position.setLocation(this.position.getX(), this.position.getY() + 1);
   }
 
+  /**
+   * Moves the character left by decrementing its X coordinate.
+   */
   public void moveLeft() {
     this.position.setLocation(this.position.getX() - 1, this.position.getY());
   }
 
+  /**
+   * Moves the character right by incrementing its X coordinate.
+   */
   public void moveRight() {
     this.position.setLocation(this.position.getX() + 1, this.position.getY());
   }
--- src/main/mvc/model/character/PacmanModel.java
+++ src/main/mvc/model/character/PacmanModel.java
@@ -2,6 +2,9 @@ package src.main.mvc.model.character;
 
 import java.awt.Point;
 
+/**
+ * Represents the Pacman character in the game.
+ */
 public class PacmanModel extends CharacterModel {
   private int lives;
 
@@ -10,10 +13,20 @@ public class PacmanModel extends CharacterModel {
     this.lives = 0;
   }
 
+  /**
+   * Returns the number of lives Pacman has.
+   *
+   * @return the number of lives Pacman has.
+   */
   public int getLives() {
     return this.lives;
   }
 
+  /**
+   * Sets the number of lives for the Pacman character.
+   * 
+   * @param lives the number of lives to set.
+   */
   public void setLives(int lives) {
     this.lives = lives;
   }
