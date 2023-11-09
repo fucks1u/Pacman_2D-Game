f35c2181314f2a5f0aa53a9151af54765ce09f58
AriirauF <ariirau.fucks@epitech.eu>
2023-11-09 14:03:51 +0100

    feat: add game panel
    This JPanel will be used for display the game

---
+++ src/main/mvc/view/panels/Game/GamePanel.java
@@ -0,0 +1,12 @@
+package src.main.mvc.view.panels.Game;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class GamePanel extends JPanel {
+    public GamePanel(){
+        this.setPreferredSize(new Dimension(800,600));
+        this.setBackground(Color.BLACK);
+        this.setVisible(true);
+    }
+}
