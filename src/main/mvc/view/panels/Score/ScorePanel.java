525ae669827d5b53a611a5504b69b7e4ec7bfd64
AriirauF <ariirau.fucks@epitech.eu>
2023-11-10 00:50:10 +0100

    feat: remove unused import
    change background color, factorize code

--- src/main/mvc/view/panels/Score/ScorePanel.java
+++ src/main/mvc/view/panels/Score/ScorePanel.java
@@ -1,9 +1,12 @@
 package src.main.mvc.view.panels.Score;
 
-import java.awt.*;
+import java.awt.Font;
+import java.awt.Color;
+import java.awt.Dimension;
 import java.io.File;
 import java.io.IOException;
 import javax.swing.*;
+import java.awt.FontFormatException;
 
 /**
  * This class is a JPanel to show the ScoreBoard of all registered users.
@@ -19,12 +22,13 @@ public class ScorePanel extends JPanel {
         JLabel title = new JLabel("ScoreBoard");
         title.setFont(customFont.deriveFont(80f));
         title.setForeground(Color.YELLOW);
+        titlepanel.setBackground(Color.BLACK);
 
         titlepanel.add(title);
 
         add(titlepanel);
-        this.setBackground(Color.BLUE);
-        this.setPreferredSize(new Dimension(800, 768));
+        this.setBackground(Color.BLACK);
+        this.setPreferredSize(new Dimension(800, 120));
     }
 
     /**
@@ -33,13 +37,11 @@ public class ScorePanel extends JPanel {
      * @return Return the custom font for PACMAN.
      */
     private Font loadCustomFont() {
-        Font customFontPacman = null;
+        Font customFontPacman;
         try {
             customFontPacman = Font.createFont(Font.TRUETYPE_FONT,
                     new File("src/main/resources/font/PAC-FONT.TTF"));
-        } catch (FontFormatException e) {
-            throw new RuntimeException(e);
-        } catch (IOException e) {
+        } catch (FontFormatException | IOException e) {
             throw new RuntimeException(e);
         }
         return customFontPacman;
