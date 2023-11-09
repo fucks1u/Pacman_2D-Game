9caefaa5532fcb50d53e16ad06b9dcd806ad5ba2
AriirauF <ariirau.fucks@epitech.eu>
2023-11-09 14:03:10 +0100

    feat: add score panel

---
+++ src/main/mvc/view/panels/Score/ScorePanel.java
@@ -0,0 +1,11 @@
+package src.main.mvc.view.panels.Score;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class ScorePanel extends JPanel {
+    public ScorePanel(){
+        this.setBackground(Color.BLUE);
+        this.setPreferredSize(new Dimension(800, 768));
+    }
+}
