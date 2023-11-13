1cec41e2e671a0908cbe47113a49f67b719e24c1
AriirauF <ariirau.fucks@epitech.eu>
2023-11-13 11:48:25 +0100

    feat: add hud panel

--- src/main/mvc/view/panels/Game/HudPanel.java
+++ src/main/mvc/view/panels/Game/HudPanel.java
@@ -3,6 +3,7 @@ package src.main.mvc.view.panels.Game;
 import java.awt.Color;
 import java.awt.Dimension;
 import javax.swing.JPanel;
+import javax.swing.BoxLayout;
 
 /**
  * This class is a JPanel to show HUD for the game.
@@ -13,7 +14,18 @@ public class HudPanel extends JPanel {
      * Constructor of the HudPanel class.
      */
     public HudPanel() {
-        this.setBackground(Color.RED);
+        this.setLayout(new BoxLayout(this, BoxLayout.X_AXIS));
+        JPanel panelCommands = new CommandsGamePanel();
+        JPanel panelScore = new ScorePanel(0,0);
+        JPanel panelLife = new JPanel();
+
+        panelLife.setPreferredSize(new Dimension(200, 170));
+        panelLife.setBackground(Color.YELLOW);
+
+        this.add(panelCommands);
+        this.add(panelScore);
+        this.add(panelLife);
+
         this.setPreferredSize(new Dimension(800, 170));
     }
 }
