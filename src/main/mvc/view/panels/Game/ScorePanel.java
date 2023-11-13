52d5d9ea2d23f797e1b488b2f4ab7f33d8228e42
AriirauF <ariirau.fucks@epitech.eu>
2023-11-13 13:50:04 +0100

    feat: add score and highscore label

--- src/main/mvc/view/panels/Game/ScorePanel.java
+++ src/main/mvc/view/panels/Game/ScorePanel.java
@@ -6,17 +6,21 @@ import java.awt.*;
 public class ScorePanel extends JPanel {
 
     public ScorePanel(int score, int highScore) {
-            setLayout(new BorderLayout());
+            setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
             setPreferredSize(new Dimension(400, 170));
-            setBackground(Color.GREEN);
+            setBackground(Color.BLACK);
 
             JLabel scorelabel = new JLabel("Score : "+ score);
-            JLabel highscorelabel = new JLabel("Highscore : "+ highScore);
-
-            scorelabel.setVerticalAlignment(SwingConstants.CENTER);
-            highscorelabel.setVerticalAlignment(SwingConstants.CENTER);
-
+            scorelabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+            scorelabel.setFont(new Font("Arial", Font.PLAIN, 30));
+            scorelabel.setBorder(BorderFactory.createEmptyBorder(0, 0, 20, 0));
+            scorelabel.setForeground(Color.WHITE);
             this.add(scorelabel);
-            this.add(highscorelabel);
+
+            JLabel highlabel = new JLabel("Highest Score : "+ highScore);
+            highlabel.setFont(new Font("Arial", Font.PLAIN, 30));
+            highlabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+            highlabel.setForeground(Color.GREEN);
+            this.add(highlabel);
         }
 }
