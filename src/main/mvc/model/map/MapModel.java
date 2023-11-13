b2e30ef604755cde87297d3d610bbb63b8e82cd6
LoubatTh <thomasloubat1@gmail.com>
2023-11-13 16:32:50 +0100

    refactor: map model

--- src/main/mvc/model/map/MapModel.java
+++ src/main/mvc/model/map/MapModel.java
@@ -1,6 +1,8 @@
 package src.main.mvc.model.map;
 
 import java.awt.Point;
+import java.util.ArrayList;
+import java.util.List;
 
 import src.main.mvc.model.item.BigDotModel;
 import src.main.mvc.model.item.DotModel;
@@ -33,10 +35,50 @@ public abstract class MapModel {
    * @return the item at the specified position
    */
   public ItemModel getCell(Point position) {
-    int posX = (int) position.getX();
-    int posY = (int) position.getY();
+    int x = (int) position.getX();
+    int y = (int) position.getY();
 
-    return this.map[posX][posY];
+    return this.map[x][y];
+  }
+
+  /**
+   * Sets the item in the specified position of the map.
+   * 
+   * @param position the position of the cell to be set.
+   * @param item     the item to be set in the cell.
+   */
+  public void setCell(Point position, ItemModel item) {
+    int x = (int) position.getX();
+    int y = (int) position.getY();
+
+    this.map[x][y] = item;
+  }
+
+  /**
+   * Sets the cell at the given position to null.
+   * 
+   * @param position the position of the cell to set
+   */
+  public void setCell(Point position) {
+    int x = (int) position.getX();
+    int y = (int) position.getY();
+
+    this.map[x][y] = null;
+  }
+
+  /**
+   * Returns a list of points representing the spawn locations on the map.
+   *
+   * @return a list of points representing the spawn locations on the map
+   */
+  public List<Point> getSpawn() {
+    List<Point> spawn = new ArrayList<>();
+    for (int i = 9; i < 19; i++) {
+      for (int j = 9; j < 19; j++) {
+        spawn.add(new Point(i, j));
+      }
+    }
+    return spawn;
   }
 
   /**
@@ -46,25 +88,31 @@ public abstract class MapModel {
    * @return true if the position is accessible, false otherwise
    */
   public boolean isAccessible(Point position) {
-    int posX = (int) position.getX();
-    int posY = (int) position.getY();
+    int x = (int) position.getX();
+    int y = (int) position.getY();
 
-    if (this.map[posX][posY] instanceof WallModel) {
+    if (this.map[x][y] instanceof WallModel) {
       return false;
     } else {
       return true;
     }
   }
 
-  public boolean isDot() {
+  /**
+   * Returns the number of dots and big dots in the map.
+   *
+   * @return the number of dots and big dots in the map
+   */
+  public int getDot() {
+    int k = 0;
     for (int i = 0; i < this.map.length; i++) {
       for (int j = 0; j < this.map[0].length; j++) {
         ItemModel item = getCell(new Point(i, j));
         if (item instanceof DotModel || item instanceof BigDotModel) {
-          return true;
+          k++;
         }
       }
     }
-    return false;
+    return k;
   }
 }
