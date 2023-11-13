00051e54a95f714627e6b244725f71b000af2612
AriirauF <ariirau.fucks@epitech.eu>
2023-11-13 13:49:35 +0100

    docs: add javadoc

--- src/main/mvc/view/panels/Game/HudPanel.java
+++ src/main/mvc/view/panels/Game/HudPanel.java
@@ -7,11 +7,16 @@ import javax.swing.BoxLayout;
 
 /**
  * This class is a JPanel to show HUD for the game.
- * It contains the HUD interface (Life, Score, etc.).
+ * It contains some panels to create a main panel.
  */
 public class HudPanel extends JPanel {
     /**
      * Constructor of the HudPanel class.
+     * It creates the JPanel and add the components.
+     * It contains the main Game frame.
+     * It contains the commands of the game.
+     * It contains the score of the game.
+     * It contains the life of the player.
      */
     public HudPanel() {
         this.setLayout(new BoxLayout(this, BoxLayout.X_AXIS));
@@ -26,6 +31,7 @@ public class HudPanel extends JPanel {
         this.add(panelScore);
         this.add(panelLife);
 
+        this.setBackground(Color.BLACK);
         this.setPreferredSize(new Dimension(800, 170));
     }
 }
