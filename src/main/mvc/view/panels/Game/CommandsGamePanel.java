5dcb86009e994014bf9d9ef4c8d608bf31444ab1
AriirauF <ariirau.fucks@epitech.eu>
2023-11-22 19:26:29 +0100

    fix: deactivate buttons
    Before, when you click a button, you can't move anymore pac-man with keys.

--- src/main/mvc/view/panels/Game/CommandsGamePanel.java
+++ src/main/mvc/view/panels/Game/CommandsGamePanel.java
@@ -26,18 +26,22 @@ public class CommandsGamePanel extends JPanel {
         JButton buttonUp = new ArrowButton("up");
         buttonUp.setBackground(Color.DARK_GRAY);
         buttonUp.setForeground(Color.YELLOW);
+        buttonUp.setEnabled(false);
         buttonUp.setBorder(BorderFactory.createLineBorder(Color.GRAY, 1));
         JButton buttonLeft = new ArrowButton("left");
         buttonLeft.setBackground(Color.DARK_GRAY);
         buttonLeft.setForeground(Color.YELLOW);
+        buttonLeft.setEnabled(false);
         buttonLeft.setBorder(BorderFactory.createLineBorder(Color.GRAY, 1));
         JButton buttonRight = new ArrowButton("right");
         buttonRight.setBackground(Color.DARK_GRAY);
         buttonRight.setForeground(Color.YELLOW);
+        buttonRight.setEnabled(false);
         buttonRight.setBorder(BorderFactory.createLineBorder(Color.GRAY, 1));
         JButton buttonDown = new ArrowButton("down");
         buttonDown.setBackground(Color.DARK_GRAY);
         buttonDown.setForeground(Color.YELLOW);
+        buttonDown.setEnabled(false);
         buttonDown.setBorder(BorderFactory.createLineBorder(Color.GRAY, 1));
 
         add(new JLabel());
