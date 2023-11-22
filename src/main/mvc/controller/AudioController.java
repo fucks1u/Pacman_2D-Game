51a668c4863e67ec0fa55391d2578457e74906e6
AriirauF <ariirau.fucks@epitech.eu>
2023-11-22 14:10:20 +0100

    docs: add javadoc

--- src/main/mvc/controller/AudioController.java
+++ src/main/mvc/controller/AudioController.java
@@ -1,9 +1,18 @@
 package src.main.mvc.controller;
 
-import javax.sound.sampled.*;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
 import java.io.File;
 import java.io.IOException;
 
+/**
+ * Class AudioController.
+ * This class is used to control the audio of the game.
+ * It has the methods to set the audio and play it.
+ * It also has the method to stop the audio.
+ * Each sound has its own method.
+ */
 public class AudioController {
     File soundFileGame;
     File soundFileGameOver;
@@ -12,6 +21,9 @@ public class AudioController {
     Clip clip;
     AudioInputStream audioInputStream;
 
+    /**
+     * Constructor of the class AudioController
+     */
     public AudioController(){
         soundFileGame = new File("src/main/resources/audio/pacman_song.wav");
         soundFileGameOver = new File("src/main/resources/audio/pacman_dead.wav");
@@ -19,6 +31,9 @@ public class AudioController {
         soundGhostVulnerable = new File("src/main/resources/audio/ghost_vulnerable.wav");
     }
 
+    /**
+     * Method to set the sound in the menu panel.
+     */
     public void setSoundGame() {
         try {
             audioInputStream = AudioSystem.getAudioInputStream(soundFileGame);
@@ -29,6 +44,9 @@ public class AudioController {
         }
     }
 
+    /**
+     * Method to set the sound when the game is over.
+     */
     public void setSoundDead() {
         try (AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(soundFileGameOver)) {
             clip = AudioSystem.getClip();
@@ -39,6 +57,9 @@ public class AudioController {
         }
     }
 
+    /**
+     * Method to set the sound when the pacman is moving.
+     */
     public void setSoundPacman(){
         try {
             audioInputStream = AudioSystem.getAudioInputStream(soundFilePacman);
@@ -49,6 +70,9 @@ public class AudioController {
         }
     }
 
+    /**
+     * Method to set the sound when the ghost is vulnerable.
+     */
     public void setSoundGhostVulnerable(){
         try {
             audioInputStream = AudioSystem.getAudioInputStream(soundGhostVulnerable);
@@ -58,14 +82,21 @@ public class AudioController {
             System.out.println(e.getMessage());
         }
     }
+
+    /**
+     * Method to play the sound.
+     */
     public void play(){
         clip.start();
     }
-    
+
+    /**
+     * Method to stop the sound.
+     * @throws IOException
+     */
     public void stop() throws IOException{
         clip.stop();
         clip.close();
         audioInputStream.close();
     }
-
 }
